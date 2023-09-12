692f8a37a643d78ada354e2774a76a32ec547ebe
Zeni <marqueszenivitor@gmail.com>
2023-09-11 21:00:43 -0300

    Update main.cpp
    Besides adding the possibility to cropp the image with a x and y coordenates given by the user and make it a .h file, the project is now able to make a variable to be used on the nokia display cotaining the values of the image bytes.

--- main.cpp
+++ main.cpp
@@ -42,11 +42,53 @@ int main()
     setlocale(LC_ALL,"");
 
     ifstream inFile; //para acessar o arquivo para leitura
-    ofstream outFile; //para aceesaor o arquivo de relatorio
+    ofstream grayscale_bmp; //para aceesaor o arquivo de relatorio
     ofstream arqmono; // arquivo de img em monocor
-    ofstream arqsaida;
-    ofstream arqtestetxt; // arquivo de teste
+    ofstream arqsaida; // arquivo que contem o vetor para ser usado no display nokia
+    ofstream corte_img; // arquivo de teste
 
+    string arquivo;// nome do arquivo
+    string sufixo;// sufixo bmp
+
+    int sufixo_pos; //posição do sufixo do arquivo
+
+    /**
+    -> Entrada do usuario do nome do arquivo
+    */
+    bool erro = true;
+    while(erro)  // enquanto o usuario não colocar o nome
+    {
+        sufixo_pos = 0;
+
+        cout << "-> DIGITE O NOME DO ARQUIVO PARA LEITURA COM O SEU TIPO:"
+             << endl << "Ex: 'nome do arquivo.tipo'" << endl;
+
+        cin >> arquivo;// usuario atribui o nome
+
+        for(int tam = arquivo.length()-1; tam > 0; --tam)  // lê o nome do arquivo inversamente para achar o ponto do tipo de arquivo
+        {
+            sufixo.push_back(arquivo[tam]); // adiciona o caractere desejado
+            sufixo_pos++;
+            if(arquivo[tam] == '.')  // se acharmos o "." do sufixo de tipo de arquivo
+            {
+                erro = false;
+                break; //fim do loop
+            }
+            else if(tam == 1)  // se não houver ".tipo"
+            {
+                cout << "---> TIPO DE ARQUIVO NAO ESPECIFICADO! TENTE NOVAMENTE <---" << endl << endl;
+                sufixo.clear(); // limpando a string para fazer o loop novamente
+                break; //fim do loop
+            }
+        }
+    }
+
+
+    // apaga o sufixo do nome do arquivo dado pelo usuario
+    arquivo.erase((arquivo.length() - sufixo_pos), arquivo.length());
+    // inverte sufixo pois foi atribuida de trás para frente
+    reverse(sufixo.begin(), sufixo.end());
+    
     inFile.open("teste.bmp", ios::in|ios::binary);
     if (!inFile) // testando se o arquivo existe
     {
@@ -89,12 +131,11 @@ int main()
     int numBytes = (cab_bit.altura_img * bytes_Linha); // numero de bytes total usados
 
     uint8_t *rgb = new uint8_t[numBytes]; // todos os bytes de todos os pixels
-    uint8_t *mono = new uint8_t[numBytes]; // todos os bytes de todos os pixels
+    uint8_t *mono = new uint8_t[numBytes]; 
     cout << "- Total de bytes: " << numBytes << endl;
 
     // gravando todos os bytes
     inFile.read((char *)rgb, numBytes);
-    inFile.read((char *)mono, numBytes);
     inFile.close();
 
 
@@ -109,18 +150,19 @@ int main()
     int controle = 0;
     while (controle != 2){
         controle = 0;
-        cout << "-> Posicao eixo X (0 a 488): ";
+        cout << "-> Posicao eixo X " << "(0 a " << cab_bit.largura_img - 84 << "): ";
         cin >> x;
-        cout << "-> Posicao eixo y (0 a 606): ";
+        cout << "-> Posicao eixo y " << "(49 a " << cab_bit.altura_img << "): ";
         cin >> y;
         cout << "-> Valor de limiar RGB (0 a 255): ";
         cin >> limiar_val;
-        //teste - dentro dos parametros
-        if(x + 84 <= cab_bit.largura_img && y - 48 > 0 && y <= cab_bit.altura_img)
+        //teste - dentro dos parametros - 49 é o minimo de linhas pq vai de 1 a 48
+        if(x + 84 <= cab_bit.largura_img && y - 49 >= 0 && y <= cab_bit.altura_img && x >= 0)
             controle++;
         if(limiar_val <= 255 && limiar_val >= 0)
             controle++;
-        if(controle != 2){
+        if(controle != 2)
+        {
             cerr << "\nERRO nos parametros" << endl;
         }
     }
@@ -134,11 +176,10 @@ int main()
     arqmono.write((char *)&cab_bit, sizeof(cabecalho_bitMapa));
 
     // escrevendo o arquivo de saida rgb para grayscale
-    outFile.open("teste_gs.bmp", ios::out);
-    outFile.write((char *)&cab_arq, sizeof(cabecalho_arq));
-    outFile.write((char *)&cab_bit, sizeof(cabecalho_bitMapa));
+    grayscale_bmp.open(arquivo + "_gs" + sufixo, ios::out);
+    grayscale_bmp.write((char *)&cab_arq, sizeof(cabecalho_arq));
+    grayscale_bmp.write((char *)&cab_bit, sizeof(cabecalho_bitMapa));
 
-    arqsaida.open("texto.h", ios::out);
     for (int y = 0; y < cab_bit.altura_img; y++) {
         for (int x = 0; x < cab_bit.largura_img; x++) {
             // calculando a posição do ponteiro rgb
@@ -167,59 +208,85 @@ int main()
         }
     }
 
-
-
-    int aux = 0;
-    int conta = 0;
-    arqtestetxt.open("teste.h", ios::out);
-    // criando o recorte da imagem
-          // redefinindo as coordenadas de inicio do ponteiro
-        for(int i = y; i > y-48; i--){
-            for (int j = x; j < x+84; j++){// inferior esquerdo para superior esquerdo
-            int byte = i * bytes_Linha + j *3;
-            int pixel = mono[byte] + mono[byte + 1] + mono[byte + 2];
-
-
-            if(pixel == 0){ //ponto ligado
-                //conta += pow(2, aux);
-                arqtestetxt << 1;
-            }else {
-                //conta += 0; // ponto desligad
-                arqtestetxt << 0;
-            }
-
-            //aux++;
-
-            }
-            arqtestetxt << endl;
-        }
-
-
-
-    //arqsaida << endl;
-
-        /*
-
-    arqsaida << "unsigned int[504] = {" << endl;
-
-    if(aux == 8) {
-               arqsaida << "0x" << conta << hex << ", ";
-               conta = 0;
-               aux = 0;
-    arqsaida << "};";
-
-    */
-    arqmono.write((char* )mono, numBytes);
+    arqmono.write((char *)mono, numBytes);
+    grayscale_bmp.write((char *)rgb, numBytes);
+    grayscale_bmp.close();
     arqmono.close();
 
-    outFile.write((char *)rgb, numBytes);
-    outFile.close();
-    arqtestetxt.close();
+    //criando um arquivo para o recorte da imagem
+    corte_img.open("arquivoteste.h", ios::out);
+
+    // criando o recorte da imagem
+    // redefinindo as coordenadas de inicio do ponteiro
+    int valores[48][84];
+    y = cab_bit.altura_img - y + 48; // invertedo a coordenada pela imagem estar ivertida
+    for(int i = y; i > y -48; i--) // como o processo é retrogrado começamos com o canto inferior esquerdo da matriz e vamos subindo
+    {
+        for (int j = 0; j < 84; j++)
+        {
+
+            int cord_x = x + j;
+
+            int byte_pos = i * bytes_Linha + cord_x *3;
+            int pixel = mono[byte_pos] + mono[byte_pos + 1] + mono[byte_pos + 2];
+
+            if(pixel == 0)  //pixel ligado na escala de mono
+            {
+                valores[y - i][j] = 1;
+            }
+            else
+            {
+                valores[y - i][j] = 0;
+            }
+
+            corte_img << valores[y - i][j]; // imprimindo a imagem de teste
+
+        }
+        corte_img << endl;
+    }
+    corte_img.close();
+
+
+    /**
+    -> Lendo e gravando os valores para a exibição no display nokia
+    */
+    arqsaida.open(arquivo + ".h", ios::out);
+    arqsaida << "uint8_t " + arquivo +"[504] = {" << endl;
+
+    //ponteiro para manipular a imagem recortada
+    int *ponteiro = &valores[0][0];
+    for(int j = 0; j < 6; j++)
+    {
+        for(int x1 = 0; x1 < 84; x1++)
+        {
+            int conta = 0;
+            for(int y1 = 0; y1 < 8; y1++)
+            {
+                if(*(ponteiro + j * (84*8) + (y1* 84) + x1) == 1)  //ponto ligado
+                {
+                    conta += pow(2, y1);// calculando o valor em hexa
+                }
+                else
+                {
+                    conta += 0;
+                }
+
+            }
+
+            if(j == 5 && x1 == 83)  // caso chegue no ultimo valor fechar as chaves da variavel
+            {
+                arqsaida << "0d" << conta << " };";
+            }
+            else arqsaida << "0d" << conta << ", ";
+
+        }
+        arqsaida << endl;
+    }
+    arqsaida.close();
 
     cout << "\nAs informacoes foram gravadas no projeto!" << endl;
     delete[] rgb;
     delete[] mono;
+    
     return 0;
-
-
     }
